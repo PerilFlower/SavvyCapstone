5ca82bda10204b9c8a173517f3e0b4fad49059fe
PerilFlower <whoviancentral10@gmail.com>
2023-10-11 15:23:19 -0500

    adding work from afternoon 10/11

---
+++ components/Header.js
@@ -0,0 +1,7 @@
+import html from "html-literal";
+
+export default state => html`
+  <header>
+    <span class="header">${state.header}</span>
+  </header>
+`;
---
+++ components/Nav.js
@@ -0,0 +1,10 @@
+xport default (links) => html`
+  <nav>
+    <ul class="hidden--mobile nav-links">
+      ${links.map(
+        (el) =>
+          `<li><a href="/${el.title}" title="${el.title}" data-navigo>${el.text}</a></li>`
+      )}
+    </ul>
+  </nav>
+`;
